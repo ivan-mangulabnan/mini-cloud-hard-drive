cbde918431a5eaac320b30d2a240ce10f9348a21
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-12-04 11:42:35 +0800

    add passport authentication

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import db from '../db/auth.js';
+
+async function verify (username, password, done) {
+  try {
+    const user = await db.getUserByUsername(username);
+    if (!user) return done(null, false, { message: 'Please re-check username' });
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return done(null, false, { message: 'Incorrect password' });
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getProfileByID(id);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(new LocalStrategy(verify));
+
+export default passport;
---
+++ db/auth.js
@@ -0,0 +1,20 @@
+import { prisma } from "../config/prisma.js";
+
+const getUserByUsername = async (username) => {
+  const user = await prisma.user.findUnique({
+    where: { username }
+  })
+
+  return user;
+}
+
+const getAccountByID = async (id) => {
+  const profile = await prisma.user.findUnique({
+    where: { id },
+    include: { profile: true }
+  })
+
+  return profile;
+}
+
+export default { getUserByUsername, getAccountByID }
