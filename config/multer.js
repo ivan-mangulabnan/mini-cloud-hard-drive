c3b8d3dde1a438baead80dbc7f408d228e199fad
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-12-09 11:55:39 +0800

    modify multer's config

--- config/multer.js
+++ config/multer.js
@@ -1,18 +1,7 @@
 import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
 
-const uploadFolder = path.join(process.cwd(), 'uploads');
-
-if (!fs.existsSync(uploadFolder)) {
-  fs.mkdirSync(uploadFolder, { recursive: true });
-}
-
-const storage = multer.diskStorage({ 
-  destination: uploadFolder,
-  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
-});
-const limits = { fileSize: 1 * 1024 * 1024 };
+const storage = multer.memoryStorage();
+const limits = { fileSize: 5 * 1024 * 1024 };
 
 const upload = multer({ storage, limits });
 
