38fd92e293cfd5c28355b66bd0a2c9e46fb7c952
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-12-09 13:17:47 +0800

    integrated supabase

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,13 +1,22 @@
 import upload from "../config/multer.js";
 import db from "../db/upload.js";
+import supabase from "../config/supabase.js";
 
 const uploadFile = () => upload.single('file')
 
 const insertFile = async (req, res) => {
+  if (!req.file) return res.redirect('/logout');
+
   let folderId = req.params.id;
   folderId = folderId ? Number(folderId) : folderId;
 
-  const file = await db.insertFile(req.user.id, req.file.filename, req.file.size, folderId);
+  const filePath = `${req.user.id}-${Date.now()}-${req.file.originalname}`;
+
+  const { error } = await supabase.storage.from('assets').upload(filePath, req.file.buffer);
+
+  if (error) return res.send('supabase error');
+
+  const file = await db.insertFile(req.user.id, req.file.originalname, req.file.size, filePath, folderId);
 
   if (!file) {
     return res.send("File isn't inserted")
--- db/upload.js
+++ db/upload.js
@@ -1,11 +1,12 @@
 import { prisma } from "../config/prisma.js";
 
-const insertFile = async (userId, fileName, size, folderId) => {
+const insertFile = async (userId, fileName, size, path, folderId) => {
   const file = prisma.file.create({
     data: {
       name: fileName,
       ownerId: userId,
       size,
+      path,
       folderId: folderId ? folderId : null
     }
   })
